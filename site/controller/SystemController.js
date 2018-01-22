f4cb0e8fbd8cef98b5582e2f254268f5e024412d
DESKTOP-V68346A\lehuy <lehuy.paul@gmail.com>
2018-01-22 23:22:19 +0700

    no message

--- site/controller/SystemController.js
+++ site/controller/SystemController.js
@@ -15,6 +15,19 @@ app.controller("SystemController", ['$scope', '$http', function($scope, $http){
             './site/api/mqtt/PublishGetSystem.php', {
             }
         ).then(function (data) { });
+
+        var resSystem = $http.post(
+            './site/api/mqtt/SubscribeGetSystem.php', {
+            }
+        ).then(function (data) { 
+            var string = data.$msg;
+            var r307 = string.substring(string.length -1, string.length);
+            var sdcard = string.substring(string.length -3, string.length - 2) 
+            var esp = string.substring(string.length -5, string.length - 4);
+            var battery = string.substring(string.length -8, string.length - 6);
+            var wifiname = string.substring(0, string.length - 9);
+        });
     }
+
     $scope.init();
 }]);
